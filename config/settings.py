9757beeb739b4a66601d89234879905b37bbf4fa
gerius <geriusfouyet@gmail.com>
2021-04-28 11:11:26 +0100

    update procfiles

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://infinite-bastion-57794.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
